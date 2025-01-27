09817db2d2cbb476194c4278da6d686823a1bdb5
Kristina Grebneva <kristina.o.grebneva@gmail.com>
2025-01-27 13:34:29 +0100

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Kristina Grebneva on 27.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
